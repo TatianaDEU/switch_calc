73f6115c46bfa33012251fe035dfb357868c3cb6
Miller Tatiana <millertatiana@MacBook-Pro.fritz.box>
2021-11-03 09:47:18 +0100

    Switch Calc

--- calc_switch.js
+++ calc_switch.js
@@ -1,36 +1,35 @@
 
 
 function calc() {
-    function isValid(numm) {
-        return numm !== undefined && typeof (numm) === 'number' && !isNaN(numm);
-    }
 
     let a = +prompt('a: ', '');
     let b = +prompt('b: ', '');
     let method = prompt('+, -, *, /', '');
-    if (isValid(a) && isValid(b)) {
-        switch (method) {
-            case '+':
-                return a + b;
-                break;
 
-            case '-':
-                return a - b;
-                break;
-
-            case '*':
-                return a * b;
-                break;
-
-            case '/':
-                if (b == 0) {
-                    alert('Error');
-                } else {
-                    return a / b;
-                }
-        }
-    } else {
+    if (isNaN(a) || isNaN(b) || a == '' || b == '' || method === '') {
         alert('Error');
     }
+    
+    switch (method) {
+        case '+':
+            alert(a + b);
+            break;
+
+        case '-':
+            alert(a - b);
+            break;
+
+        case '*':
+            alert(a * b);
+            break;
+
+        case '/':
+            if (b == 0) {
+                alert('Error');
+            } else {
+                alert(a / b);
+            }
+    }
 }
+
 calc();
